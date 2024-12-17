19c6b80644059397e96a91fcb58bd205b15ed76a
flashthought <146617030+flashthought@users.noreply.github.com>
2024-12-17 16:39:01 +0000

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -19,7 +19,7 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title="Check my github profile"
+              title="Github profile"
               href="https://github.com/flashthought"
             >
               <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] lg:h-[30vh] mb-10">
