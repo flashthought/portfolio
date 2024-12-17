074de126fd9908bd4d7f6fdbcc18a8b1938e9832
flashthought <146617030+flashthought@users.noreply.github.com>
2024-12-17 16:23:32 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,3 @@
 const nextConfig = {
   /* config options here */
 };
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  nextConfig,
-  {
-    org: "flashcode",
-    project: "javascript-nextjs",
-    silent: !process.env.CI,
-    widenClientFileUpload: true,
-    reactComponentAnnotation: {
-      enabled: true,
-    },
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-  }
-);
